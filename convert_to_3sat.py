773c404af9bb40014609c2d9168b379eaafdf105
cynthi8 <coldegret@protonmail>
2023-05-17 02:01:50 -0400

    feat: don't expand clauses

--- convert_to_3sat.py
+++ convert_to_3sat.py
@@ -1,6 +1,10 @@
 import os
 import sys
 
+class VariableCounter:
+    def __init__(self, value=0):
+        self.value = value
+
 def read_CNF(cnf_file_path):
     clauses = []
     with open(cnf_file_path, 'r') as file:
@@ -25,10 +29,13 @@ def write_CNF(clauses, cnf_file_path):
 def largest_variable(clauses):
     return max([abs(variable) for clause in clauses for variable in clause])
 
-def split_clause(clause, new_glue_variable):
+def split_clause(clause, free_var):
     if len(clause) <= 3:
         return clause, None
     
+    new_glue_variable = free_var.value
+    free_var.value += 1
+
     clause3 = [clause[0], clause[1], new_glue_variable]
     clause_rest = [-new_glue_variable] + clause[2:]
     return clause3, clause_rest
@@ -38,25 +45,25 @@ def expand_clause(clause):
         clause.append(clause[0])
     return clause
     
-def convert_clauses_to_3sat(clauses):
+def convert_clauses_to_3sat(clauses, expand = False):
     final_clauses = []
 
-    largest_var = largest_variable(clauses)
-    free_var = largest_var + 1
+    free_var = VariableCounter(largest_variable(clauses) + 1)
 
     for clause in clauses:
-        clause_left, clause_right = split_clause(clause, free_var)
-        final_clauses.append(expand_clause(clause_left))
+        clause_left, clause_right = None, clause
         while clause_right:
-            free_var += 1
             clause_left, clause_right = split_clause(clause_right, free_var)
-            final_clauses.append(expand_clause(clause_left))
+            if expand:
+                final_clauses.append(expand_clause(clause_left))
+            else:
+                final_clauses.append(clause_left)
 
     return final_clauses
 
 def convert_to_3sat(cnf_file_path):
     clauses = read_CNF(cnf_file_path)
-    new_clauses = convert_clauses_to_3sat(clauses)
+    new_clauses = convert_clauses_to_3sat(clauses, False)
     output_file_path = os.path.splitext(cnf_file_path)[0] + "_3sat.cnf"
     write_CNF(new_clauses, output_file_path) 
 
